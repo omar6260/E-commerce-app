e32df2f171c713d80ebebbaad24251cab73f5f86
Oumar Fall <foumar404@gmail.com>
2022-09-18 00:23:59 +0100

    continue your implement app

---
+++ lib/src/presentation/pages/categorie_page/category_product.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CategoryProduct extends StatefulWidget {
+  CategoryProduct({Key? key}) : super(key: key);
+
+  @override
+  State<CategoryProduct> createState() => _CategoryProductState();
+}
+
+class _CategoryProductState extends State<CategoryProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/presentation/pages/register_page/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPge extends StatefulWidget {
+  LoginPge({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPge> createState() => _LoginPgeState();
+}
+
+class _LoginPgeState extends State<LoginPge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
